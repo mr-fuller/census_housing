42ac51252359c903ecee68d37b5b153f21a5d9e8
fullerm <mrfuller460@gmail.com>
2018-06-01 16:40:41 -0400

    create base_dir and use as parameter;

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import pandas
+import pandas,os
 from housingunitdata import housingunitdata
 from movedatatoexcel import movedatatoexcel
 
+year_int = 2016
+print('  Building directory structure on Z:\...'),  # add a line to handle exceptions?
+#acs_year = str(year_int - 4) + 'to' + str(year_int)[-2:]
+base_dir = "Z:/fullerm/LRP/Housing/" + str(year_int)
+# Create base directory if it doesn't exist
+if not os.path.exists(base_dir):
+    os.makedirs(base_dir)
 df = housingunitdata()
-movedatatoexcel(df)
+movedatatoexcel(base_dir,df)
 
